1f2ba1ffd1360d6f20163c3e96e691a29db8e754
Urshamunikar <ursha.munikar12@gmail.com>
2021-01-20 16:19:07 +0545

    files added

--- src/components/Services.js
+++ src/components/Services.js
@@ -1,12 +1,61 @@
-import React from 'react'
+import React, { Component } from 'react'
+import { FaCocktail, FaHiking, FaBeer, FaShuttleVan } from 'react-icons/fa'
 
-const Services = () => {
-    return (
-        <div>
-            <h4 className="section-title">Services</h4>
-            <div></div>
-        </div>
-    )
+class Services extends Component  {
+    state = {
+        services: [
+            {
+                icon: <FaCocktail />,
+                title: "Free Cocktail",
+                info: "Get Free Cocktail as welcome "
+
+            },
+
+            {
+                icon: <FaHiking />,
+                title: "Endless Hiking",
+                info: "Note that the development build is not optimized.To create a production build, use npm run build. "
+
+            },
+            {
+                icon: <FaShuttleVan />,
+                title: "Free Drive",
+                info: "Note that the development build is not optimizedTo create a production build, use npm run build."
+
+            },
+            {
+                icon: <FaBeer />,
+                title: "Free Beer",
+                info: "Note that the development build is not optimized To create a production build, use npm run build."
+
+            }
+        ]
+    }
+    render(){
+        return (
+            <section className="services">
+                <div className="section-title">
+                    <h4>Services</h4>
+                    <div></div>
+                </div>
+                <div className="services-center">
+                   { this.state.services.map((item, index) => {
+                    return(
+                        <article className="service" key={index}>
+                            <span>{item.icon}</span>
+                            <h6>{item.title}</h6>
+                            <p>{item.info}</p>
+                        </article>
+                        )
+
+                    }) }
+    
+                </div>
+            </section>
+         )
+    }
+  
+   
 }
 
 export default Services;
